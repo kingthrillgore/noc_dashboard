70bda333ff3a5fba9fa6b09f9b2ee863467b9eec
Cameron Kilgore <ghostfreeman@gmail.com>
2016-10-07 22:53:23 -0400

    Moving things to use the wunderground XML

--- js/config.js
+++ js/config.js
@@ -6,5 +6,6 @@
 nocDashboard.constant("SETTINGS", {
   "NAGIOS_STATUS_PAGE" : "index.html",
   "CAP_ATOM_FEED" : "https://alerts.weather.gov/cap/wwaatmget.php?x=GAZ033&y=0",
-  "WEATHER_POSTAL_CODE": 30076
+  "LOCATION_POSTAL_CODE": 30076,
+  "WUNDERGROUND_API_KEY": "N/A"
 });
