c05ba23fdc71b15f0397c26e716d536b4eaaf42e
Cameron Kilgore <ghostfreeman@gmail.com>
2016-10-20 22:04:27 -0400

    Implemented the tab-switching logic.

--- js/main/tabs/controllers/tabsController.js
+++ js/main/tabs/controllers/tabsController.js
@@ -3,13 +3,32 @@
  * @author Cameron Kilgore
  */
 
-nocDashboard.controller("tabsController", function($scope, $localStorage, $sessionStorage, $interval, Popeye) {
+nocDashboard.controller("tabsController", function($scope, $localStorage, $sessionStorage, $interval, $route, $routeParams, $location, Popeye) {
+  $scope.tabs = {
+    validPaths: [],
+    numberOfMenuItems: 0,
+    currentActiveItem: 0,
+    settings: {
+      cycleTime: 0,
+      showLoading: false,
+    },
+    initalized: false
+  };
+
+  $scope.$on('$viewContentLoaded', function () {
+    console.log("Tabs view loaded");
+
+    if($scope.tabs.initalized !== true) {
+      $scope.prepForScreenCycle();
+    }
+  });
+
   $scope.pageInit = function() {
     //Initalize Settings from existing LocalStorage. Otherwise, switch to defaults
     if(typeof $localStorage.settings !== undefined) {
-      $scope.settings = $localStorage.settings;
+      $scope.tabs.settings = $localStorage.settings;
     } else {
-      $scope.settings.cycleTime = 10;
+      $scope.tabs.settings.cycleTime = 10;
     }
 
     $scope.cycleThroughScreens();
@@ -29,9 +48,9 @@ nocDashboard.controller("tabsController", function($scope, $localStorage, $sessi
       }
     });
 
-    $scope.showLoading = true;
+    $scope.tabs.settings.showLoading = true;
     SettingsModal.resolved.then(function() {
-      $scope.showLoading = false;
+      $scope.tabs.showLoading = false;
     });
 
     SettingsModal.closed.then(function() {
@@ -43,7 +62,7 @@ nocDashboard.controller("tabsController", function($scope, $localStorage, $sessi
   $scope.saveApplicationSettings = function() {
     //Validate settings
     if(typeof $scope.settings.cycleTime === undefined) {
-      $scope.settings.cycleTime = 10;
+      $scope.tabs.settings.cycleTime = 10;
     }
 
     //TODO if still set to do so, re-initalize cycleThroughScreens()
@@ -55,9 +74,23 @@ nocDashboard.controller("tabsController", function($scope, $localStorage, $sessi
     Popeye.closeCurrentModal();
   };
 
+  $scope.prepForScreenCycle = function() {
+    if($scope.tabs.initalized !== true) {
+      var menuItems = document.querySelectorAll('.navigation ul li a[data-url]');
+      $scope.tabs.numberOfMenuItems = menuItems.length - 1;
+
+      angular.forEach(menuItems, function(value, key) {
+        if(typeof value.getAttribute('data-url') !== undefined) {
+          $scope.tabs.validPaths.push(value.getAttribute('data-url'));
+        }
+      });
+
+      $scope.tabs.initalized = true;
+    }
+  };
 
   $scope.cycleThroughScreens = function() {
-    var intervalTimerMillisec = $scope.settings.cycleTime * 1000;
+    var intervalTimerMillisec = $scope.tabs.settings.cycleTime * 1000;
 
     $interval(
       function() {
@@ -66,10 +99,16 @@ nocDashboard.controller("tabsController", function($scope, $localStorage, $sessi
   };
 
   $scope.changeScreen = function() {
-    //TODO Get all rules from the router
+    var destination = "";
 
-    //TODO Switch to the next page
-
-    //console.debug("SetInterval firing", moment());
+    if($scope.tabs.currentActiveItem < $scope.tabs.numberOfMenuItems) {
+      $scope.tabs.currentActiveItem++;
+      destination = "/"+$scope.tabs.validPaths[$scope.tabs.currentActiveItem];
+      $location.path(destination);
+    } else {
+      $scope.tabs.currentActiveItem = 0;
+      destination = "/"+$scope.tabs.validPaths[$scope.tabs.currentActiveItem];
+      $location.path(destination);
+    }
   };
 });
