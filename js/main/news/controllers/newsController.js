ff12c8cf21dfaf2f7eee0b353d0fdaaf67626adf
Cameron Kilgore <ghostfreeman@gmail.com>
2016-10-22 00:28:12 -0400

    Initial work on the News page (and am now forced to address the server because of all these fucking CORS issues)

--- js/main/news/controllers/newsController.js
+++ js/main/news/controllers/newsController.js
@@ -3,8 +3,14 @@
  * @author Cameron Kilgore
  */
 
-nocDashboard.controller("newsController", function($scope) {
+nocDashboard.controller("newsController", function($scope, newsFactory, settings) {
   $scope.pageInit = function() {
+    $scope.getHeadlines();
+  };
 
+  $scope.getHeadlines = function() {
+    //newsFactory.getLatestHeadlines("http://rss.cnn.com/rss/cnn_topstories.rss");
+    //newsFactory.getLatestHeadlines("http://anyorigin.com/get?url=http%3A//rss.cnn.com/rss/cnn_topstories.rss&callback=?");
+    console.log("Call made");
   };
 });
