f3821292876cf41b44cc06b780ce1e877b6206b0
Cameron Kilgore <ghostfreeman@gmail.com>
2016-10-22 22:40:56 -0400

    changes made to implement news page and gitignore

--- js/main/news/controllers/newsController.js
+++ js/main/news/controllers/newsController.js
@@ -8,9 +8,18 @@ nocDashboard.controller("newsController", function($scope, newsFactory, settings
     $scope.getHeadlines();
   };
 
-  $scope.getHeadlines = function() {
-    //newsFactory.getLatestHeadlines("http://rss.cnn.com/rss/cnn_topstories.rss");
-    //newsFactory.getLatestHeadlines("http://anyorigin.com/get?url=http%3A//rss.cnn.com/rss/cnn_topstories.rss&callback=?");
+  $scope.getHeadlines = function(numHeadlines) {
+    //Validation
+    if(typeof numHeadlines !== "number") {
+      numHeadlines = 10;
+    }
+
     console.log("Call made");
+    newsFactory.getLatestHeadlines("http://rss.cnn.com/rss/cnn_topstories.rss", true).then(function(SuccessResponse) {
+      console.debug("SuccessResponse", SuccessResponse);
+      //Trim Items down to number specified.
+    }, function(ErrorResponse) {
+      console.debug("ErrorResponse", ErrorResponse);
+    });
   };
 });
--- js/main/news/services/newsFactory.js
+++ js/main/news/services/newsFactory.js
@@ -6,23 +6,32 @@
 nocDashboard.factory("newsFactory", function($http, $q) {
   var service = {};
 
-  service.getLatestHeadlines = function(NewsFeedURL) {
+  service.getLatestHeadlines = function(NewsFeedURL, EnablePassthrough) {
     //Validation
     if(typeof NewsFeedURL !== 'string') {
       return false;
     }
 
+    if(typeof EnablePassthrough !== 'boolean') {
+      EnablePassthrough = false;
+    }
+
     var deferred = $q.defer();
+
+    //If Passthrough is being used, build that GET request
+    if(EnablePassthrough) {
+      NewsFeedURL = "http://127.0.0.1:5000/passthrough_xml?url="+NewsFeedURL;
+    }
 
     //Get the latest XML Feed
     $http.get(NewsFeedURL).then(function(SuccessResponse) {
       var x2js = new X2JS();
-      ProcessedFeed = x2js.xml_str2json(SuccessResponse);
+      ProcessedFeed = x2js.xml_str2json(SuccessResponse.data);
 
       console.debug("SuccessResponse", SuccessResponse);
-      console.debug("ProcessedFeed", ProcessedFeed);
+      console.debug("ProcessedFeed", ProcessedFeed.rss.channel);
 
-      deferred.resolve(ProcessedFeed);
+      deferred.resolve(ProcessedFeed.rss.channel);
     }, function(ErrorResponse) {
       console.debug("Error", ErrorResponse);
     });
